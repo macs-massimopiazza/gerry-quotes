f2662de4b25f017daadd936b344e6fe24fb8518d
macs-massimo-piazza <me@massimopiazza.com>
2024-12-18 19:00:32 +0100

    feat: add docs page

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const routerQuotes = require('./routers/gerryQuotes');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-  res.send('Magica api per gerry. Endpoints: /gerryQuotes/all , /gerryQuotes/random');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 app.use('/quotes', routerQuotes);
 
