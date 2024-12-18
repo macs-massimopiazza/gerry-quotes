a5695c5fee7c0eb4fcf1afefadd6188e11fa5261
macs-massimo-piazza <me@massimopiazza.com>
2024-12-18 18:39:07 +0100

    feat: edit endpoint base

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routerQuotes = require('./routers/gerryQuotes');
 app.get('/', (req, res) => {
   res.send('Magica api per gerry. Endpoints: /gerryQuotes/all , /gerryQuotes/random');
 })
-app.use('/gerryQuotes', routerQuotes);
+app.use('/quotes', routerQuotes);
 
 app.listen(3000, () => {
   console.log("Listening to port 3000");
