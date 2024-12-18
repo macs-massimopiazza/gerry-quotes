11b7fa4e187e1a39d577f3db877b33ebba6edc16
macs-massimo-piazza <me@massimopiazza.com>
2024-12-18 18:33:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const routerQuotes = require('./routers/gerryQuotes');
+app.get('/', (req, res) => {
+  res.send('Magica api per gerry. Endpoints: /gerryQuotes/all , /gerryQuotes/random');
+})
+app.use('/gerryQuotes', routerQuotes);
+
+app.listen(3000, () => {
+  console.log("Listening to port 3000");
+})
