d763ef8e0ac5837525a6fe0a445ac2fe31bc12a4
macs-massimo-piazza <me@massimopiazza.com>
2024-12-19 07:18:28 +0100

    feat: minify and get image from thrid party

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const routerQuotes = require('./routers/gerryQuotes');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
+});
 app.use('/quotes', routerQuotes);
 
 app.listen(3000, () => {
